187d2c154b56c29b5799401b734099546ca62992
Antoine Poincare <Giovanni.Antonio.Carl.Friedrich@protonmail.com>
2019-12-04 11:22:20 +0300

    fmt added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <filesystem>
+#include <fmt/format.h>
+
+namespace fs = std::filesystem;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    using namespace std::string_literals;
+    using namespace fmt::literals;
+    auto extension = ".mkv"s;
+    for(const auto& p: fs::recursive_directory_iterator(fs::current_path()))
+    {
+        if (p.path().string().ends_with(extension))
+        {
+            fmt::print("{mkv} has {size} bytes\n",
+                        "mkv"_a  = p.path().filename().string(),
+                        "size"_a = 42);
+        }
+    }
     return 0;
 }
